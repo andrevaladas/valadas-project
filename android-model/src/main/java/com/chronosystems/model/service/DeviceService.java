3bb18f875f24246499db6e31df640abb523c979e
andrevaladas <andrevaladas@gmail.com>
2012-05-04 06:56:24 +0000

    MySql dialect defined for pagination search.

--- android-model/src/main/java/com/chronosystems/model/service/DeviceService.java
+++ android-model/src/main/java/com/chronosystems/model/service/DeviceService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import com.chronosystems.entity.Device;
 import com.chronosystems.entity.Entity;
+import com.chronosystems.entity.Location;
 import com.chronosystems.model.dao.DeviceDao;
 
 /**
@@ -19,27 +20,40 @@ public class DeviceService extends DeviceDao implements Serializable {
 	private static final long serialVersionUID = -1626723954167785038L;
 
 	@Override
-	public Device find(String email) {
+	public Device find(final String email) {
 		return super.find(email);
 	}
-	
+
 	@Override
-	public Device find(String email, String password) {
+	public Device find(final String email, final String password) {
 		return super.find(email, password);
 	}
-	
+
 	@Override
-	public void save(Device entity) {
+	public void save(final Device entity) {
 		super.save(entity);
 	}
-	
+
 	@Override
-	public Long rowCount(String email) {
+	public Long rowCount(final String email) {
 		return super.rowCount(email);
 	}
-	
+
 	@Override
 	public Entity search(final Entity entity) {
-		return super.search(entity);
+		final Entity resultEntity = super.search(entity);
+
+		// return only last device location
+		for (final Device device : resultEntity.getDevices()) {
+			device.setDateInTime(device.getDatecreated().getTime());
+			final List<Location> locations = device.getLocations();
+			for (final Location location : locations) {
+				location.setDateInTime(location.getTimeline().getTime());
+			}
+			//only last location
+			//device.setLocations(new ArrayList<Location>());
+			//device.addLocation(location);
+		}
+		return resultEntity;
 	}
 }
