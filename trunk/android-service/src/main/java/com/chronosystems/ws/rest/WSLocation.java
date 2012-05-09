601378d17baff950434ab6f1442aaa329c826b65
andrevaladas@gmail.com <andrevaladas@gmail.com@1d1d5d60-a9c2-c088-f220-44a90a2b1b1c>
2012-05-09 10:10:20 +0000

    Chechin de localização pelo GPS; Google Places - testes; Google Directions - testes; Compressão de imagem; Cache de imagens;

---
+++ trunk/android-service/src/main/java/com/chronosystems/ws/rest/WSLocation.java
@@ -0,0 +1,35 @@
+package com.chronosystems.ws.rest;
+
+import java.util.Date;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.chronosystems.entity.Device;
+import com.chronosystems.entity.Entity;
+import com.chronosystems.entity.Location;
+import com.chronosystems.entity.util.XMLParser;
+import com.chronosystems.model.service.DeviceService;
+
+@Path("/location")
+public class WSLocation {
+
+	@POST
+	@Path("/checkin")
+	@Produces(MediaType.TEXT_XML)
+	public Response login(final String xml) {
+		final Device filter = XMLParser.parseXML(xml, Device.class);
+		final Device device = new DeviceService().find(filter.getId());
+
+		final Location checkin = filter.getLocations().get(0);
+		checkin.setTimeline(new Date());
+		checkin.setDevice(device);
+		device.addLocation(checkin);
+		new DeviceService().save(device);
+
+		return Response.ok(XMLParser.parseXML(new Entity(device))).build();
+	}
+}
