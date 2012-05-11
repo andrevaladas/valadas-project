411f681e98ba908954f97a18122e73c3471366bd
andrevaladas <andrevaladas@1d1d5d60-a9c2-c088-f220-44a90a2b1b1c>
2012-05-11 22:20:29 +0000

    Jelastic + Xeround

--- android-device/src/com/chronosystems/service/remote/ServerAddress.java
+++ android-device/src/com/chronosystems/service/remote/ServerAddress.java
@@ -3,7 +3,9 @@ package com.chronosystems.service.remote;
 public enum ServerAddress {
 	//http://andrevaladas.appspot.com/rest/device/get/andrevaladas
 	LOCAL("http://10.0.2.2:8888/android-service/rest"),
-	REMOTE("http://192.168.0.101:8888/android-service/rest");
+	//REMOTE("http://192.168.0.101:8888/android-service/rest");
+	//REMOTE("http://andrevaladas.appspot.com/rest");
+	REMOTE("http://chronosystems.jelastic.servint.net/android-service/rest");
 
 	private String url;
 
