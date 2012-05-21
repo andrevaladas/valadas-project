9cecdd4814eb90b9a48d95b29b8f9f3531321e61
andrevaladas@gmail.com <andrevaladas@gmail.com@1d1d5d60-a9c2-c088-f220-44a90a2b1b1c>
2012-05-21 07:18:27 +0000

    Ajustes

--- examples/BindSpinner/src/com/gone/bindspinner/BindSpinnerActivity.java
+++ examples/BindSpinner/src/com/gone/bindspinner/BindSpinnerActivity.java
@@ -11,54 +11,38 @@ import android.widget.Spinner;
 
 public class BindSpinnerActivity extends Activity {
 	static final String[] COUNTRIES = new String[] { "India", "US", "UK",
-			"Sri Lanka", "Austraila", "Denmark", "Saudi" };
+		"Sri Lanka", "Austraila", "Denmark", "Saudi" };
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		/*
 		 * Binding static Array to the spinner
-		 * 
 		 */
-		Spinner spCountry = (Spinner) findViewById(R.id.spCounty);
-
-		ArrayAdapter<CharSequence> adCountry = new ArrayAdapter<CharSequence>(
-				this, android.R.layout.simple_spinner_item, COUNTRIES);
-
-		adCountry
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		final Spinner spCountry = (Spinner) findViewById(R.id.spCounty);
+		final ArrayAdapter<CharSequence> adCountry = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, COUNTRIES);
+		adCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spCountry.setAdapter(adCountry);
 
 		/*
-		 * Binding resource Array to the Spinner
-		 * Approach 1
+		 * Binding resource Array to the Spinner Approach 1
 		 */
-		Spinner spCities = (Spinner) findViewById(R.id.spCities);
-
-		String[] cities = getResources().getStringArray(R.array.arr_cities);
-
-		ArrayAdapter<CharSequence> adCities = new ArrayAdapter<CharSequence>(
-				this, android.R.layout.simple_spinner_item, cities);
+		final Spinner spCities = (Spinner) findViewById(R.id.spCities);
+		final String[] cities = getResources().getStringArray(R.array.arr_cities);
+		final ArrayAdapter<CharSequence> adCities = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, cities);
 
 		adCities.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spCities.setAdapter(adCities);
 
 		/*
-		 * Binding resource Array to the Spinner
-		 * Approach 2
+		 * Binding resource Array to the Spinner Approach 2
 		 */
-		Spinner spLocalities = (Spinner) findViewById(R.id.spLocality);
-
-		ArrayAdapter<CharSequence> adLocalities = ArrayAdapter
-				.createFromResource(this, R.array.arr_localities,
-						android.R.layout.simple_spinner_item);
-
-		adLocalities
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+		final Spinner spLocalities = (Spinner) findViewById(R.id.spLocality);
+		final ArrayAdapter<CharSequence> adLocalities = ArrayAdapter.createFromResource(this, R.array.arr_localities,android.R.layout.simple_spinner_item);
+		adLocalities.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spLocalities.setAdapter(adLocalities);
 	}
 }
