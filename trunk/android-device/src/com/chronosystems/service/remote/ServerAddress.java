c175aba3a5c0c86796ef1c37c6b19d877c086532
andrevaladas@gmail.com <andrevaladas@gmail.com@1d1d5d60-a9c2-c088-f220-44a90a2b1b1c>
2012-05-21 07:21:54 +0000

    remote server

--- trunk/android-device/src/com/chronosystems/service/remote/ServerAddress.java
+++ trunk/android-device/src/com/chronosystems/service/remote/ServerAddress.java
@@ -2,8 +2,8 @@ package com.chronosystems.service.remote;
 
 public enum ServerAddress {
 	//REMOTE("http://10.0.2.2:8888/android-service/rest");
-	REMOTE("http://192.168.0.101:8888/android-service/rest");
-	//REMOTE("http://chronosystems.jelastic.servint.net/android-service/rest");
+	//REMOTE("http://192.168.0.101:8888/android-service/rest");
+	REMOTE("http://chronosystems.jelastic.servint.net/android-service/rest");
 
 	private String url;
 
