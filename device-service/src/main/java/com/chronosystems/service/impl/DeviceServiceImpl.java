397f7a944130ff403372c7dba78c6dc12111f9ba
andrevaladas <andrevaladas@gmail.com>
2012-05-22 07:27:33 +0000

    Configuranto para buscar as ultimas localizações 10 de cada device.

--- device-service/src/main/java/com/chronosystems/service/impl/DeviceServiceImpl.java
+++ device-service/src/main/java/com/chronosystems/service/impl/DeviceServiceImpl.java
@@ -1,5 +1,9 @@
 package com.chronosystems.service.impl;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,13 +11,18 @@ import org.springframework.transaction.annotation.Transactional;
 import com.chronosystems.dao.DeviceDAO;
 import com.chronosystems.entity.Device;
 import com.chronosystems.entity.Entity;
+import com.chronosystems.entity.Location;
 import com.chronosystems.service.DeviceService;
+import com.chronosystems.service.LocationService;
 
 @Service
 public class DeviceServiceImpl implements DeviceService {
 
 	@Autowired
 	private DeviceDAO deviceDAO;
+
+	@Autowired
+	private LocationService locationService;
 
 	@Override
 	@Transactional
@@ -54,12 +63,32 @@ public class DeviceServiceImpl implements DeviceService {
 	@Override
 	@Transactional
 	public Entity findFollowing(final Entity entity) {
-		return deviceDAO.findFollowing(entity);
+		final Entity entityResult = deviceDAO.findFollowing(entity);
+		loadLastLocations(entityResult);
+		return entityResult;
 	}
 
 	@Override
 	@Transactional
 	public Entity findFollowers(final Entity entity) {
 		return deviceDAO.findFollowers(entity);
+	}
+
+	private void loadLastLocations(final Entity entityResult) {
+		// return only last device location
+		final List<Device> devices = entityResult.getDevices();
+		for (final Device device : devices) {
+			device.setDateInTime(device.getDatecreated().getTime());
+			final List<Location> locations = locationService.findLastLocations(device.getId());
+			device.setLocations(locations);
+		}
+
+		//order by lastLocation
+		Collections.sort(devices, new Comparator<Device>() {
+			@Override
+			public int compare(final Device o1, final Device o2) {
+				return o2.getLocations().get(0).getTimeline().compareTo(o1.getLocations().get(0).getTimeline());
+			};
+		});
 	}
 }
